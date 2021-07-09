41d13528bef60994453243ca3e9750586370e159
morarafrank <morarafrank11@gmail.com>
2021-07-09 19:18:23 +0300

    layouts inflation

--- app/src/main/java/com/frankmorara/navish/ExitFragment.kt
+++ app/src/main/java/com/frankmorara/navish/ExitFragment.kt
@@ -1,6 +1,16 @@
 package com.frankmorara.navish
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.frankmorara.navish.databinding.FragmentExitBinding
 
 class ExitFragment: Fragment(R.layout.fragment_exit) {
+    lateinit var binding: FragmentExitBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentExitBinding.inflate(layoutInflater)
+        return binding.root
+    }
 }
--- app/src/main/java/com/frankmorara/navish/SearchFragment.kt
+++ app/src/main/java/com/frankmorara/navish/SearchFragment.kt
@@ -1,7 +1,18 @@
 package com.frankmorara.navish
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.frankmorara.navish.databinding.FragmentSearchBinding
 
 class SearchFragment: Fragment(R.layout.fragment_search) {
+    lateinit var binding: FragmentSearchBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentSearchBinding.inflate(layoutInflater)
+        return binding.root
+
+    }
 
 }
