6c205e72c62514906d81f1aeeedd973b654bd4da
morarafrank <morarafrank11@gmail.com>
2021-07-09 19:15:44 +0300

    inflating layouts

--- app/src/main/java/com/frankmorara/navish/SettingsFragment.kt
+++ app/src/main/java/com/frankmorara/navish/SettingsFragment.kt
@@ -1,6 +1,17 @@
 package com.frankmorara.navish
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.frankmorara.navish.databinding.FragmentSettingsBinding
 
 class SettingsFragment: Fragment(R.layout.fragment_settings) {
+    private lateinit var binding: FragmentSettingsBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentSettingsBinding.inflate(layoutInflater)
+        return binding.root
+
+    }
 }
--- app/src/main/java/com/frankmorara/navish/TermsFragment.kt
+++ app/src/main/java/com/frankmorara/navish/TermsFragment.kt
@@ -1,6 +1,17 @@
 package com.frankmorara.navish
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.frankmorara.navish.databinding.FragmentTermsBinding
 
 class TermsFragment: Fragment(R.layout.fragment_terms) {
+    lateinit var binding: FragmentTermsBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentTermsBinding.inflate(layoutInflater)
+        return binding.root
+
+    }
 }
